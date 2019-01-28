d7aa8e8b7597d238870b6b1fc89eb8a5de87382f
Goren G <yong.gu@qlink.mobi>
2019-01-28 15:24:20 +0800

    feat: support profile cpu and mem
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -2,7 +2,14 @@ package commands
 
 import (
 	"fmt"
+	"github.com/qlcchain/go-qlc/common/util"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+	"runtime"
+	"runtime/pprof"
+	"time"
 
 	"github.com/qlcchain/go-qlc/common/types"
 	"github.com/qlcchain/go-qlc/config"
@@ -10,14 +17,16 @@ import (
 	"github.com/qlcchain/go-qlc/chain"
 	"github.com/qlcchain/go-qlc/common"
 	"github.com/spf13/cobra"
+	_ "net/http/pprof"
 )
 
 var (
-	account  string
-	pwd      string
-	cfgPath  string
-	ctx      *chain.QlcContext
-	services []common.Service
+	account   string
+	pwd       string
+	isProfile bool
+	cfgPath   string
+	ctx       *chain.QlcContext
+	services  []common.Service
 )
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -60,6 +69,52 @@ func start() error {
 			fmt.Println(err)
 		}
 	}
+
+	if isProfile {
+		profDir := filepath.Join(cfg.DataDir, "pprof", time.Now().Format("2006-01-02T15-04"))
+		_ = util.CreateDirIfNotExist(profDir)
+		//CPU profile
+		cpuProfile, err := os.Create(filepath.Join(profDir, "cpu.prof"))
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		} else {
+			log.Println("create CPU profile: ", cpuProfile.Name())
+		}
+
+		runtime.SetCPUProfileRate(500)
+		if err := pprof.StartCPUProfile(cpuProfile); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		} else {
+			log.Println("start CPU profile")
+		}
+		defer func() {
+			_ = cpuProfile.Close()
+			pprof.StopCPUProfile()
+		}()
+
+		//MEM profile
+		memProfile, err := os.Create(filepath.Join(profDir, "mem.prof"))
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		} else {
+			log.Println("create MEM profile: ", memProfile.Name())
+		}
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(memProfile); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		} else {
+			log.Println("start MEM profile")
+		}
+		defer func() {
+			_ = memProfile.Close()
+		}()
+
+		go func() {
+			//view result in http://localhost:6060/debug/pprof/
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
 	err = runNode(addr, pwd, cfg)
 	if err != nil {
 		return err
@@ -74,4 +129,5 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&cfgPath, "config", "c", "", "config file")
 	rootCmd.PersistentFlags().StringVarP(&account, "account", "a", "", "wallet address,if is nil,just run a node")
 	rootCmd.PersistentFlags().StringVarP(&pwd, "password", "p", "", "password for wallet")
+	rootCmd.PersistentFlags().BoolVar(&isProfile, "profile", false, "enable profile")
 }
